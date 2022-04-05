182ef51bdb04d357fc37b2ab823e7fdd21066eae
vikasprogrammer <vikas.programmer@gmail.com>
2022-04-05 15:17:39 +0530

    Update hello-world.php

--- hello-world.php
+++ hello-world.php
@@ -30,7 +30,7 @@ if(!defined('ABSPATH')) {
 class HelloWorld_Plugin {
 
 	/*
-	 * Consructors are magical functions
+	 * Consructors are magical functions v1
 	 * They are executed when you instantiate a object.
 	 * It initializes our object.
 	 * Call your function inside Consrtuctor to execute immediately.
