65d9be36c81ecffa71091c42b4d15afbb81703d5
vikasprogrammer <vikas.programmer@gmail.com>
2022-12-01 09:56:46 +0530

    Update hello-world.php

--- hello-world.php
+++ hello-world.php
@@ -4,7 +4,7 @@
  *
  * @package hello-world
  *
- * Plugin Name: InstaWP Github Integration Demo 
+ * Plugin Name: InstaWP Hello World Demo
  * Plugin URI: https://instawp.io
  * Description: A plugin to greet the visitors or just another test plugin to be honest.
  * Version: 1.2.2
@@ -27,35 +27,18 @@ if(!defined('ABSPATH')) {
  * @class HelloWorld_Plugin
  */
 
-class HelloWorld_Plugin {
 
-	/* 
-	 * Consructors are magical functions
-	 * They are executed when you instantiate a object.
-	 * It initializes our object.
-	 * Call your function inside Consrtuctor to execute immediately.
-	 * Use $this for the reference of an object
-	 */
-
-	function __construct() {
-		// executes our congratulations function.
-		//adding a new line of comment ;-)
-		$this->congratulations();
-	}
-
-	// Congratulations function
-
-	function congratulations() {
-
-		echo "<!-- 1.0.2 Congratulations, you have just learned basics OOP and Plugin Development using Object Oriented Programming.-->";
-
-	}
-
+add_action('admin_menu', 'hello_world_admin_menu');
+function hello_world_admin_menu() {
+  add_menu_page(
+        'Hello World',// page title
+        'Hello World',// menu title
+        'manage_options',// capability
+        'hello-world',// menu slug
+        'display_hello_world_page' // callback function
+    );
 }
 
-
-// check if our class exists
-if(class_exists('HelloWorld_Plugin')) {
-	// instantiate the class.
-	$hellWorld_plugin = new HelloWorld_Plugin();
+function display_hello_world_page() {
+  echo 'Hello World!';
 }
